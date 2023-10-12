3a21dbb24af68d72d1e03f13314e16eabbefd799
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-10-12 14:12:04 +0700

    h

--- src/Entity.php
+++ src/Entity.php
@@ -1,10 +1,12 @@
 <?php
-
 /**
  * @return
  * @author Phạm Quang Linh <linhpq@getflycrm.com>
- * @since 12/10/2023 2:01 pm
+ * @since 12/10/2023 2:11 pm
  */
+
+namespace phamquanglinhdev\Laptrinhluon;
+
 trait Entity
 {
 
